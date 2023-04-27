26d0e1094adedb7616250abb15fa0cae39e01027
Glaydson Pacifico <glaydson.pacifico@gmail.com>
2023-04-27 17:13:50 -0300

    small adjustment function share whatsapp

--- frontend/src/components/GalleryItem/index.tsx
+++ frontend/src/components/GalleryItem/index.tsx
@@ -1,4 +1,5 @@
 import { AiOutlineWhatsApp } from 'react-icons/ai';
+import api from '../../utils/api';
 import { Container } from './styles';
 
 interface GalleryItemProps {
@@ -13,10 +14,9 @@ export function GalleryItem({ title, image, artistId }: GalleryItemProps) {
     const urlToShorten = encodeURIComponent(image);
     const apiUrl = `https://api.shrtco.de/v2/shorten?url=${urlToShorten}`;
 
-    fetch(apiUrl)
-      .then(response => response.json())
-      .then(data => {
-        const shortUrl = data.result.full_short_link;
+    api.get(apiUrl)
+      .then(response => {
+        const shortUrl = response.data.result.full_short_link;
         const message = `Confira este produto: ${title} - ${shortUrl}`;
         const phone = '558196216405';
         const url = `https://api.whatsapp.com/send?phone=${phone}&text=${message}`;
@@ -26,6 +26,7 @@ export function GalleryItem({ title, image, artistId }: GalleryItemProps) {
         console.error('Erro ao encurtar URL:', error);
       });
   }
+
   return (
     <Container imgUrl={image}>
       <a>
